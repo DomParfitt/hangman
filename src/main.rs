bec48a84df088b9087fa46fa99ae5f6030c7ed03
Dom Parfitt <dom.parfitt@gmail.com>
2018-01-23 21:11:27 +0000

    Began implementing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+
+    let answer = "answer";
+    let mut guessed_letters: Vec<&str> = Vec::new();
+
+    print_current_guess(&guessed_letters, answer);
+    get_user_guess(&guessed_letters);
+
+
+}
+
+fn print_current_guess(&guessed_letters: Vec<&str>, answer: &str) {
+    for idx in 0..answer.len() {
+        let letter = &answer[idx..idx+1];
+        if guessed_letters.contains(&letter) {
+            print!("{}", letter);
+        } else {
+            print!("_");
+        }
+        print!(" ");
+    }
+}
+
+fn get_user_guess(&mut guessed_letters: Vec<&str>) {
+
+    let mut input = String::new();
+    let stdin = io::stdin();
+    match stdin.lock().read_line(&mut input) {
+        Ok(_) => {
+            let letter = &input[0..1];
+            if !guessed_letters.contains(&letter) {
+                guessed_letters.push(letter);
+            }
+        },
+        Err(_) => {
+            println!("Please enter a letter.")
+        },
+    }
 }
